357616e1cd9ab8ebe4491d268829e4a9006cea13
slacku <bananuya3@gmail.com>
2023-03-17 13:26:53 -0300

    Desarrollo apartado e punto 3

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,11 @@
 
 void main()
 {
-    printf("Hola mundo!");
+    int a = 69, *pa;
+    pa = &a;
+    printf("Contenido del puntero: %d\n", *pa);
+    printf("Direccion de memoria almacenada por el puntero: %d\n", pa);
+    printf("Direccion de memoria de la variable a: %d\n", &a);
+    printf("Direccion de memoria del puntero: %d\n", &pa);
+    printf("Espacio que ocupa en memoria la variable a: %d\n", sizeof(a));
 }
